9fe2f9a4ef12632f45b4a9d261789a6300d2f575
Blake Johnson <blake.t.johnson-1@ou.edu>
2024-09-20 10:45:02 -0500

    test file to run tests on the programs

---
+++ L5_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
+from scipy.optimize import root
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import sympy as sp
+import sys
+
+
+from part_5 import *
